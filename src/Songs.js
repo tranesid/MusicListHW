8cbe094cae754eb7e3e5054c72f02148d413f283
Sidney <tranesidney@gmail.com>
2022-05-31 15:15:38 -0600

    errors grrrr

---
+++ src/Songs.js
@@ -0,0 +1,8 @@
+class Songs = [
+  { id: 1, name: "Freaky Deaky", artist: "Doja Cat"},
+  { id: 2, name: "Kiss Me More", artist: "Doja Cat"},
+  { id: 3, name: "Five Hours", artist: "Deorro"},
+  { id: 4, name: "Somebody Else", artist: "The 1975"},
+]
+
+export default Songs;
---
+++ src/Table.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Songs from "./Songs";
+
+class MusicList {
+
+renderMusicList=()=>{
+  // return this.props.songs.map(s=> )
+}
+render() {
+  return (
+    <Songs />
+  );
+}};
+
+export default MusicList;
